18332147ebf20aa049b938c48f9a2b2ea5bb3e40
Tim Wiese <tim.wiese@esa.int>
2020-02-04 17:58:00 +0100

    Add ROS 2 logging

--- src/Logging.hpp
+++ src/Logging.hpp
@@ -1,4 +1,10 @@
+#pragma once
+
+#ifdef ROCK
 #include "base-logging/Logging.hpp"
+#elif ROS2
+#include "rclcpp/rclcpp.hpp"
+#endif
 
 using namespace platform_driver_ethercat;
 
@@ -9,19 +15,39 @@ inline void log(const LogLevel level, const std::string context, const std::stri
     switch (level)
     {
         case LogLevel::DEBUG:
+            #ifdef ROCK
             LOG_DEBUG_S << context << message;
+            #elif ROS2
+            RCLCPP_DEBUG(rclcpp::get_logger(context), "%s", message.c_str());
+            #endif
         break;
         case LogLevel::INFO:
+            #ifdef ROCK
             LOG_INFO_S << context << message;
+            #elif ROS2
+            RCLCPP_INFO(rclcpp::get_logger(context), "%s", message.c_str());
+            #endif
         break;
         case LogLevel::WARN:
+            #ifdef ROCK
             LOG_WARN_S << context << message;
+            #elif ROS2
+            RCLCPP_WARN(rclcpp::get_logger(context), "%s", message.c_str());
+            #endif
         break;
         case LogLevel::ERROR:
+            #ifdef ROCK
             LOG_ERROR_S << context << message;
+            #elif ROS2
+            RCLCPP_ERROR(rclcpp::get_logger(context), "%s", message.c_str());
+            #endif
         break;
         case LogLevel::FATAL:
+            #ifdef ROCK
             LOG_FATAL_S << context << message;
+            #elif ROS2
+            RCLCPP_FATAL(rclcpp::get_logger(context), "%s", message.c_str());
+            #endif
         break;
     }
 }
